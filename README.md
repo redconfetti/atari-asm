8420f1e670c8ff1155f80942d547016ea5db4588
Jason Miller <jason@redconfetti.com>
2019-12-07 09:38:10 -0800

    Adds README and cleanmemory example

---
+++ README.md
@@ -0,0 +1,6 @@
+# Atari 2600 Assembly Experiments
+
+Assembly for the MOS 6507 microprocessor, taken from
+[Learn Assembly Language by Making Games for the Atari 2600]
+
+[learn assembly language by making games for the atari 2600]: https://www.udemy.com/course/programming-games-for-the-atari-2600/
---
+++ atari/cleanmemory/cleanmem.asm
@@ -0,0 +1,28 @@
+  processor 6502
+  seg code
+  org $F000   ; define the code origin at $F000
+
+Start:
+  sei         ; disable interrupts
+  cld         ; disable the BCD decimal math mode
+  ldx #$FF    ; loads the X register with #$FF
+  txs         ; transfer X register to S(tack) register
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Clear the Zero Page region ($00 to $FF)
+; Meaning the entire TIA register space and also RAM
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  lda #0      ; A = 0
+  ldx #$FF    ; X = #$FF
+
+MemLoop:
+  sta $0,X    ; store zero at address $0 + X
+  dex         ; x--
+  bne MemLoop ; loop until X==0 (z-flag set)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fill ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  org $FFFC
+  .word Start ; reset vector at $FFFC (where program starts)
+  .word Start ; interrupt vector at $FFFE (unused in Atari VCS)
